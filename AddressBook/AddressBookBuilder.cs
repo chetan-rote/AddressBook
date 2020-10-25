282e44a024a38a1c29121c2addd41e1b6bb74d52
Chetan <Chetan@DESKTOP-9SU09BS>
2020-10-25 16:26:03 +0530

    [Chetan]Add.Edit contact by name.

--- AddressBook/AddressBookBuilder.cs
+++ AddressBook/AddressBookBuilder.cs
@@ -38,6 +38,56 @@ namespace AddressBook
             Email = Console.ReadLine();
             Contacts contact = new Contacts(FirstName, LastName, Address, City, State, Zip, PhoneNumber, Email);
             dictionary.Add(FirstName, contact);
-        }        
+        }
+        /// <summary>
+        /// Displays the contacts.
+        /// </summary>
+        public void displayContacts()
+        {
+            foreach (KeyValuePair<string, Contacts> dict in dictionary)
+            {
+                Console.WriteLine("First Name : " + dict.Value.firstName);
+                Console.WriteLine("Last Name : " + dict.Value.lastName);
+                Console.WriteLine("Address : " + dict.Value.address);
+                Console.WriteLine("City : " + dict.Value.city);
+                Console.WriteLine("State : " + dict.Value.state);
+                Console.WriteLine("Zip : " + dict.Value.zip);
+                Console.WriteLine("Phone Number : " + dict.Value.phoneNo);
+                Console.WriteLine("Email : " + dict.Value.email);
+            }
+        }
+        /// <summary>
+        /// Edits the contact.
+        /// </summary>
+        /// <param name="firstName">The first name.</param>
+        public void editContact(string firstName)
+        {
+            int found = 0;
+            foreach (KeyValuePair<string, Contacts> dict in dictionary)
+            {
+                if (firstName.Equals(dict.Key))
+                {
+                    found = 1;
+                    Console.WriteLine("Enter Last Name : ");
+                    dict.Value.lastName = Console.ReadLine();
+                    Console.WriteLine("Enter Address: ");
+                    dict.Value.address = Console.ReadLine();
+                    Console.WriteLine("Enter City : ");
+                    dict.Value.city = Console.ReadLine();
+                    Console.WriteLine("Enter State : ");
+                    dict.Value.state = Console.ReadLine();
+                    Console.WriteLine("Enter Zip code : ");
+                    dict.Value.zip = Console.ReadLine();
+                    Console.WriteLine("Enter Phone Number : ");
+                    dict.Value.phoneNo = Console.ReadLine();
+                    Console.WriteLine("Enter Email : ");
+                    dict.Value.email = Console.ReadLine();
+                }
+            }
+            if (found == 0)
+            {
+                Console.WriteLine("No Contacts matched with given first name");
+            }
+        }
     }
 }
