622f87a4f7b1639a6d89a034d1de8d865773b696
Chetan <Chetan@DESKTOP-9SU09BS>
2020-10-25 16:08:40 +0530

    [Chetan]Add.Create contact.

---
+++ AddressBook/AddressBookBuilder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    class AddressBookBuilder
+    {
+        public Dictionary<string, Contacts> dictionary;
+        public AddressBookBuilder()
+        {
+            this.dictionary = new Dictionary<string, Contacts>();
+        }
+    }
+}
